ed0e4f493ddc3a0307d769462abb9875e35c6229
max <hennighausenmax@gmail.com>
2022-05-01 18:40:00 +0200

    added sidebar & topbar & app.css, modified App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import Sidebar from './components/sidebar/Sidebar';
+import Topbar from './components/topbar/Topbar';
 import { Home } from './pages/home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Home />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,52 @@
+import { LineStyle, Timeline } from '@material-ui/icons';
+// import { Link } from 'react-router-dom';
+import './sidebar.css';
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            {/* <Link to="/" className="link"> */}
+            <li className="sidebarListItem">
+              <LineStyle className="sidebarIcon" />
+              home
+            </li>
+            {/* </Link> */}
+            {/* <Link to="/products" className="link"> */}
+            <li className="sidebarListItem">
+              <LineStyle className="sidebarIcon" />
+              products
+            </li>
+            {/* </Link> */}
+            {/* <Link to="/users" className="link"> */}
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" />
+              users
+            </li>
+            {/* </Link> */}
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <LineStyle className="sidebarIcon" />
+              home
+            </li>
+            <li className="sidebarListItem">
+              <LineStyle className="sidebarIcon" />
+              analytics
+            </li>
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" />
+              sales
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
